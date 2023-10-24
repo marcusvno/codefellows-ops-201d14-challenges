11fbdb32475f3706614b9ebab164241950d0e219
marcusvno <marcusnogueira23@gmail.com>
2023-10-24 18:27:49 -0400

    Hello World script challenge completed. Tried out some variables for added practice.

---
+++ 201-ops/helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Marcus Nogueira
+# Date of latest revision:      10/24/2023
+# Purpose:                      Print Hello World in CLI.
+
+# Declaration of variables
+greeting="Hello World!"
+statement="I am working perfectly!"
+# Declaration of functions
+
+
+# Main
+echo $greeting
+echo $statement
+# End
