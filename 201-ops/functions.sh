b8968cdef7aced1e41e465ce9fd1c5c1e888cdfa
marcusvno <marcusnogueira23@gmail.com>
2023-10-25 16:23:44 -0400

    Created bash template and finished Functions challenge.

---
+++ 201-ops/functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script Name:                  Functions
+# Author:                       Marcus Nogueira
+# Date of latest revision:      10/25/2023
+# Purpose:                      Prints the login history of this system. Repeat 3x.
+
+# Declaration of variables
+loginHistory(){
+   last
+   echo "This is the login history."
+}
+# Declaration of functions
+
+
+# Main
+
+loginHistory
+loginHistory
+loginHistory
+
+# End
