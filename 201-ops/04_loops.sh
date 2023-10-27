be95d7e9ec96e2a5b24ef51d2e1fc58199f1696f
marcusvno <marcusnogueira23@gmail.com>
2023-10-27 16:18:55 -0400

    Used ChatGpt to give feedback to the user when kill executes successful or not.

--- 201-ops/04_loops.sh
+++ 201-ops/04_loops.sh
@@ -15,9 +15,12 @@ kill_process(){
     ps aux
     echo #line break
     read -p "Enter PID: " process_id
-    kill -9 $process_id
-    read -p "Continue? (y/n): " continue_killing
-
+    if kill -9 $process_id 2>/dev/null; then
+      echo "Successfully killed process with PID $process_id."
+    else
+      echo "Failed to kill process with PID $process_id. It may not exist or you may lack necessary permissions."
+    fi
+    read -p "Kill another process? (y/n): " continue_killing
     if [[ $continue_killing == "n" || $continue_killing == "N" ]]; then
       break
     fi
@@ -28,3 +31,6 @@ kill_process(){
 # Main
 kill_process
 # End
+
+# Citation: ChatGPT helped with explaining the usage of "2>/dev/null" for successful command execution in shell scripting. 
+# Citation: Used https://www.cyberciti.biz/faq/bash-while-loop/ to simplify writing the infinite loop
