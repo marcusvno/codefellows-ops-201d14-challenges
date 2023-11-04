786d4f7359be5b138db136d1cd1c3d02c6963dcc
marcusvno <marcusnogueira23@gmail.com>
2023-11-04 02:10:50 -0400

    Started challenge for class 10.

---
+++ 201-ops/09_system_process.ps1
@@ -0,0 +1,13 @@
+# Script Name:                  SCRIPT NAME
+# Author:                       Marcus Nogueira
+# Date of latest revision:      MM/DD/YYYY
+# Purpose:                      PURPOSE
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+# End
