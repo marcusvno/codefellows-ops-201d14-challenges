9525d8360e2cea03e15e2416f333225fbd9fa554
Marcus Nogueira <112592295+marcusvno@users.noreply.github.com>
2023-11-04 05:28:02 -0400

    Update 10_system_process.ps1
    Updating challenge 10 to meet stretch goals.

--- 201-ops/10_system_process.ps1
+++ 201-ops/10_system_process.ps1
@@ -1,13 +1,43 @@
-# Script Name:                  SCRIPT NAME
+# Script Name:                  System Process Commands
 # Author:                       Marcus Nogueira
-# Date of latest revision:      MM/DD/YYYY
-# Purpose:                      PURPOSE
+# Date of latest revision:      11/03/2023
+# Purpose:                      Analyze, initialize, and terminate processes using PowerShell
 
-# Declaration of variables
+#Print to the terminal screen all active processes ordered by highest CPU time consumption at the top.
+function CPU_SORT { 
+  Get-Process | Sort-Object CPU -Descending 
+}
 
-# Declaration of functions
+#Print to the terminal screen all active processes ordered by highest Process Identification Number at the top.
+function ID_SORT {
+  Get-Process | Sort-Object ID -Descending 
+}
 
+#Print to the terminal screen the top five active processes ordered by highest Working Set (WS(K)) at the top.
+function TOP5_WS {
+  Get-Process | Sort-Object WS -Descending | Select-Object -First 5
+}
 
-# Main
+#Start a browser process (such as Google Chrome or MS Edge) and have it open https://owasp.org/www-project-top-ten/.
+function CHROME_URL {
+  Start-Process "chrome.exe" -ArgumentList "https://owasp.org/www-project-top-ten/"
+}
 
-# End
+#Start the process Notepad ten times using a for loop.
+function OPEN_PROCESS{
+    for ($i = 0; $i -lt $args[0]; i++) {
+    Start-Process "notepad.exe"
+  }
+}
+
+#Close all instances of the Notepad.
+Stop-Process -Name notepad
+
+#Kill a process by its Process Identification Number. Choose a process whose termination won’t destabilize the system, such as Google Chrome or MS Edge.
+Stop-Process -Id 512
+
+#Stretch Goals:
+#Embed each task above into its own function.
+#Create a menu which lets the user choose between each function.
+
+function
