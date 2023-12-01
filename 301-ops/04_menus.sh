4e4c97c59363c789798c6a0744483b71f2e815d2
marcusvno <marcusnogueira23@gmail.com>
2023-12-01 01:48:48 -0500

    Completed Ops 301 Code Challenge 04 on conditionals and menus.

---
+++ 301-ops/04_menus.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Script Name:                  menus
+# Author:                       Marcus Nogueira
+# Date of latest revision:      11/30/2023
+# Purpose:                      PURPOSE
+# Resources Used:               https://unix.stackexchange.com/questions/487141/get-every-ip-except-for-the-loopback-device 
+
+
+# Declaration of variables
+
+# Declaration of functions
+function menu(){
+
+  while :
+    do
+      clear
+      echo ""
+      echo "  -------------------------------"
+      echo "  |            Menu             |"
+      echo "  -------------------------------"
+      echo "  | 1. Hello                    |"
+      echo "  | 2. Ping Self                |"
+      echo "  | 3. IP Info                  |"
+      echo "  | 4. Quit                     |"
+      echo "  -------------------------------"
+      read -p " Enter option: " menuChoice
+
+      
+      case $menuChoice in
+        1) helloworld;;
+        2) ping_self;;
+        3) ip_info;;
+        4) break;;
+        *) echo " Invalid choice";;
+      esac
+
+      read -p " Press ENTER to return to menu."
+    done
+    quit_menu
+   # read -p " Press ENTER to return to menu."
+  }
+
+function helloworld(){
+  clear
+  figlet "Hello World" -f "DOS Rebel" | lolcat --animate --speed 60
+}
+
+function ping_self() {
+  clear
+  figlet "Ping Self" -f "ANSI Shadow" | lolcat --animate --speed 60
+  ping -c 5 localhost
+  echo ""
+  return
+}
+
+function ip_info(){
+  clear
+  figlet "ip info" -w 120 -f Fraktur | lolcat --animate --speed 70
+  ip a show scope global
+  echo ""
+}
+
+function quit_menu(){
+  clear 
+  figlet -c "See ya space cowboy" -f "Rowan Cap" | lolcat
+}
+
+# Main
+menu
+
+# End
