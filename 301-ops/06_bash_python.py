c46e00f1d9d138ecdc61d90fb36752f842dd1dfd
marcusvno <marcusnogueira23@gmail.com>
2023-12-04 17:35:24 -0500

    Formatted with autopep8

--- 301-ops/06_bash_python.py
+++ 301-ops/06_bash_python.py
@@ -18,16 +18,17 @@ def snake_bash():
     os.system('figlet "Punching Snakes" -f "3d" | lolcat')
     print("\n\n")
     time.sleep(1)
-    
+
     print(f'{BLUE}LOGGED IN USER: {RESET}')
     os.system('whoami')
-    print ("")
+    print("")
     time.sleep(1)
-    ip_output = subprocess.getoutput("ip a show eno1 | grep -oP 'inet \K\d{1,3}(\.\d{1,3}){3}'")
+    ip_output = subprocess.getoutput(
+        "ip a show eno1 | grep -oP 'inet \K\d{1,3}(\.\d{1,3}){3}'")
     print(f'{BLUE}CURRENT IP ADDRESS:\n{RESET}{ip_output}')
     print("")
     time.sleep(1)
-    
+
     print(f'{BLUE}THIS PC\'S HARDWARE:{RESET}')
     os.system('sudo lshw -short')
 
