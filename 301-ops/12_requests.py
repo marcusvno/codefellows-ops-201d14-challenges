06cadc44afc9a34399c24dfdc7a2dcbd637dce81
marcusvno <marcusnogueira23@gmail.com>
2023-12-12 23:55:06 -0500

    301 Ops Challenge 12: missed the headers output. Added it. Cleaned it up with a for loop print.

--- 301-ops/12_requests.py
+++ 301-ops/12_requests.py
@@ -51,79 +51,89 @@ def confirmation(url, method):
         match method:
             case "GET":
                 response = req.get(url, timeout=30)
-                return response.status_code
+                return response
             case "POST":
                 response = req.post(url, timeout=30)
-                return response.status_code
+                return response
             case "PUT":
                 response = req.put(url, timeout=30)
-                return response.status_code
+                return response
             case "DELETE":
                 response = req.delete(url, timeout=30)
-                return response.status_code
+                return response
             case "HEAD":
                 response = req.head(url, timeout=30)
-                return response.status_code
+                return response
             case "PATCH":
                 response = req.patch(url, timeout=30)
-                return response.status_code
+                return response
             case "OPTIONS":
                 response = req.options(url, timeout=30)
-                return response.status_code
+                return response
     else:
         print("Canceling operation.")
         return "cancel"
 
 
 def print_status(response_code):
-    if response_code == 200:
-        print("Response code:", response_code,
+    
+    print("")
+    
+    if response_code.status_code == 200:
+        print("Response code:", response_code.status_code,
               "OK - The request was successful.")
-    elif response_code == 201:
-        print("Response code:", response_code,
+    elif response_code.status_code == 201:
+        print("Response code:", response_code.status_code,
               "Created - The request was successful, and a new resource was created.")
-    elif response_code == 204:
-        print("Response code:", response_code,
+    elif response_code.status_code == 204:
+        print("Response code:", response_code.status_code,
               "No Content - The request was successful, but there is no additional information to send back.")
-    elif response_code == 400:
-        print("Response code:", response_code,
+    elif response_code.status_code == 400:
+        print("Response code:", response_code.status_code,
               "Bad Request - The request could not be understood or was missing required parameters.")
-    elif response_code == 401:
-        print("Response code:", response_code,
+    elif response_code.status_code == 401:
+        print("Response code:", response_code.status_code,
               "Unauthorized - Authentication failed or user does not have permissions.")
-    elif response_code == 403:
-        print("Response code:", response_code,
+    elif response_code.status_code == 403:
+        print("Response code:", response_code.status_code,
               "Forbidden - The server refuses to authorize the request.")
-    elif response_code == 404:
-        print("Response code:", response_code,
+    elif response_code.status_code == 404:
+        print("Response code:", response_code.status_code,
               "Not Found - The requested resource could not be found.")
-    elif response_code == 405:
-        print("Response code:", response_code,
+    elif response_code.status_code == 405:
+        print("Response code:", response_code.status_code,
               "Method Not Allowed - The specified method is not allowed for the resource.")
-    elif response_code == 500:
-        print("Response code:", response_code,
+    elif response_code.status_code == 500:
+        print("Response code:", response_code.status_code,
               "Internal Server Error - An unexpected condition was encountered on the server.")
-    elif response_code == 502:
-        print("Response code:", response_code,
+    elif response_code.status_code == 502:
+        print("Response code:", response_code.status_code,
               "Bad Gateway - The server received an invalid response from the upstream server.")
-    elif response_code == 503:
-        print("Response code:", response_code,
+    elif response_code.status_code == 503:
+        print("Response code:", response_code.status_code,
               "Service Unavailable - The server is not ready to handle the request.")
-    elif response_code == 504:
-        print("Response code:", response_code,
+    elif response_code.status_code == 504:
+        print("Response code:", response_code.status_code,
               "Gateway Timeout - The server did not receive a timely response from the upstream server.")
-    elif response_code == 301:
-        print("Response code:", response_code,
+    elif response_code.status_code == 301:
+        print("Response code:", response_code.status_code,
               "Moved Permanently - The requested resource has been permanently moved.")
-    elif response_code == 302 or response_code == 303:
-        print("Response code:", response_code,
+    elif response_code.status_code == 302 or response_code.status_code == 303:
+        print("Response code:", response_code.status_code,
               "Found/See Other - The requested resource resides temporarily under a different URL.")
-    elif response_code == 304:
-        print("Response code:", response_code,
+    elif response_code.status_code == 304:
+        print("Response code:", response_code.status_code,
               "Not Modified - The resource has not been modified since the specified version.")
     else:
-        print("Your response code is", response_code,
+        print("Your response code is", response_code.status_code,
               "which is not defined in this Python script.")
+        exit()
+
+    print("")
+    for key, value in response_code.headers.items():
+        print(f" -{key}: {value}")
+
+    # print(response_code.headers)
 
 
 # Main Block
