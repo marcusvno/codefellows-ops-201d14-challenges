3db847f13a36086fa1cfb23b53cc4404c46838ff
marcusvno <marcusnogueira23@gmail.com>
2024-01-10 02:02:11 -0500

    Begun 401 Ops Challenge 02 Python Ping Sensor Tool.

---
+++ 401-ops/02_sensor_tool.py
@@ -0,0 +1,22 @@
+'''
+This is an Uptime Sensor Tool using ICMP packet pings and Python to determine if hosts on a LAN are up or down. 
+
+REQUIREMENTS:
+  - Transmit a single ICMP (ping) packet to a specific IP every two seconds.
+  - Evaluate the response as either success or failure.
+  - Assign success or failure to a status variable.
+  - For every ICMP transmission attempted, print the status variable along with a comprehensive timestamp and destination IP tested.
+  
+  Example output: 
+    2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
+'''
+
+
+def user_ping():
+    user_input = input("Enter ping to test: ")
+    return user_input
+
+
+
+if __name__ == "__main__":
+    ping_address = user_ping()
