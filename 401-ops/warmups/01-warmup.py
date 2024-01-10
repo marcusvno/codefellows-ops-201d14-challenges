062aedba0cead1059e08e77a3b7a3eb1fa6d6682
marcusvno <marcusnogueira23@gmail.com>
2024-01-10 01:24:21 -0500

    401 Ops Challenges started.

---
+++ 401-ops/warmups/01-warmup.py
@@ -0,0 +1,9 @@
+# Prompt user for Text
+word = input("\nEnter a word: ")
+
+# Prompt user for number
+number = int(input('Enter a number: '))
+
+# Print Input X number of times
+for x in range(number):
+    print(word)
