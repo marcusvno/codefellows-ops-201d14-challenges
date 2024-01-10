0740ec0723619b8d026fe83bc58d43d65192a88b
marcusvno <marcusnogueira23@gmail.com>
2024-01-10 08:05:17 -0500

    Finished 401 Ops Challenge 02 and stretch goals.

--- 401-ops/02_sensor_tool.py
+++ 401-ops/02_sensor_tool.py
@@ -9,19 +9,35 @@ REQUIREMENTS:
   
   Example output: 
     2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
-'''
-#!/usr/bin/env python3
 
-import datetime
-import os
+RESOURCES: 
+https://github.com/alessandromaggio/pythonping
+https://web.archive.org/web/20190814203701/https://www.ictshore.com/python/python-ping-tutorial/
+'''
+
+#!/usr/bin/python3
+
+from datetime import datetime
+import time
+from pythonping import ping
+
 
 def user_ping():
-    user_input = input("Enter ping to test: ")
+    user_input = input("Enter IPv4 address: ")
     return user_input
 
-def test_ping(address):
-  
 
+def test_ping(ip_address):
+    start_time = datetime.now()
+    filename=f'{start_time.strftime("%Y-%m-%d_%H:%M:%S")}_ping_log_{ip_address}.txt'
+    with open(f'{filename}', 'a') as file:
+        while True:
+            response = ping(ip_address, count=1)
+            now = datetime.now()
+            status = f'{now.strftime("%Y-%m-%d_%H:%M:%S")} Network ACTIVE to {ip_address}' if response.success() else f'{now.strftime("%Y-%m-%d_%H:%M:%S")} Network INACTIVE to {ip_address}'
+            print(status)
+            file.write(f'{status}\n')
+            time.sleep(2)
 
 if __name__ == "__main__":
     ping_address = user_ping()
