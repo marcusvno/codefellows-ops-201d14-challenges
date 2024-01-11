bac461ebd30c1096bb145cad0157cf1c6007c60c
marcusvno <marcusnogueira23@gmail.com>
2024-01-11 10:27:33 -0500

    Updating 401 Ops Challenge 03

--- 401-ops/03_ping_email.py
+++ 401-ops/03_ping_email.py
@@ -10,33 +10,60 @@ RESOURCES:
 https://github.com/alessandromaggio/pythonping
 https://web.archive.org/web/20190814203701/https://www.ictshore.com/python/python-ping-tutorial/
 https://www.tutorialspoint.com/How-to-print-current-date-and-time-using-Python
-https://towardsdatascience.com/how-to-easily-automate-emails-with-python-8b476045c151
+https://archive.is/Tl9bj#selection-1833.0-1859.44
+
+
+REQUIRED PACKAGES TO INSTALL:
+pythonping
+python-dotenv
+
 '''
 
 #!/usr/bin/python3
 
-from datetime import datetime
-import time
-from pythonping import ping
+import os #Used with dotenv
+from datetime import datetime #For timestamps
+import time #To space out our pings
+from dotenv import load_dotenv #Loads in our environment variables
+from pythonping import ping #Used to ping our targets
+import smtplib 
+import ssl 
+from email.message import EmailMessage
 
+load_dotenv() #Load in environment variables
 
 def user_ping():
     user_input = input("Enter IPv4 address: ")
     return user_input
 
+def receiver():
+    user_input = input("Enter target email for notifications: ")
+    return user_input
 
-def test_ping(ip_address):
+def send_email(receiver_email, subject, body):
+    message = EmailMessage()
+    message["From"] = os.getenv('EMAIL_ADDRESS')
+
+def test_ping(ip_address, notification_email):
+    """Pythonping requires elevated permissions or it will error out. Run with sudo."""
+    
     start_time = datetime.now()
-    filename=f'{start_time.strftime("%Y-%m-%d_%H:%M:%S")}_ping_log_{ip_address}.txt'
+    filename = f'{start_time.strftime("%Y-%m-%d_%H:%M:%S")}_ping_log_{ip_address}.txt'
+    
     with open(f'{filename}', 'a') as file:
         while True:
             response = ping(ip_address, count=1)
             now = datetime.now()
-            status = f'{now.strftime("%Y-%m-%d_%H:%M:%S")} Network ACTIVE to {ip_address}' if response.success() else f'{now.strftime("%Y-%m-%d_%H:%M:%S")} Network INACTIVE to {ip_address}'
-            print(status)
-            file.write(f'{status}\n')
+            timestamp = now.strftime("%Y-%m-%d_%H:%M:%S")
+            # status = f'{now.strftime("%Y-%m-%d_%H:%M:%S")} Network ACTIVE to {ip_address}' if response.success(
+            status = "ACTIVE" if response.success() else "INACTIVE" #Cleaner version than above
+            print(f'{timestamp} - Network {status} - {ip_address}')
+            file.write(f'{timestamp} - Network {status} - {ip_address}\n')
             time.sleep(2)
 
+
 if __name__ == "__main__":
+       
     ping_address = user_ping()
-    test_ping(ping_address)
+    notification_addr = receiver()
+    test_ping(ping_address, notification_addr)
