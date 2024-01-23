78ef28e54e7260347681c1733c6af7889943835d
marcusvno <marcusnogueira23@gmail.com>
2024-01-23 09:52:01 -0500

    Port input added.

--- 401-ops/11_scapy_v1.py
+++ 401-ops/11_scapy_v1.py
@@ -16,27 +16,54 @@
 #   - If flag 0x14 received, notify user the port is closed.
 #   - If no flag is received, notify the user the port is filtered and silently dropped.
 
-# import scapy
+# from scapy.all import sr1, IP, ICMP
 
-target = "scanme.nmap.org"
-print(f'Default target: {target}')
+def target_prompt():
+    target = "scanme.nmap.org"
+    print(f'\nDefault target: {target}')
+    while True:  # loop for first prompt
+        change_target = input("Do you wish to change target [Y/N]: ").lower()
 
-while True:  # loop for first prompt
-    change_target = input("Do you wish to change target [Y/N]: ").lower()
+        if change_target == 'y':
+            while True:  # loop for entering a new target
+                new_target = input("Enter new target: ")
+                print(f'New target: {new_target}')
+                continue_input = input(
+                    "Continue with this target? [Y/N] ").lower()
 
-    if change_target == 'y':
-        while True:  # loop for entering a new target
-            new_target = input("Enter new target: ")
-            print(f'New target: {new_target}')
-            continue_input = input("Continue with this target? [Y/N] ").lower()
+                if continue_input.lower() == 'y':
+                    target = new_target  # update the target variable
+                    print(f'\nCurrent target: {target}')
+                    return target  # breaks out of the inner loop
 
-            if continue_input == 'y':
-                target = new_target  # update the target variable
-                break  # breaks out of the inner loop
+            # break  # break out of the outer loop
 
-        break  # break out of the outer loop
+        elif change_target == 'n':
+            print(f'\nCurrent target: {target}')
+            return target
 
-    elif change_target == 'n':
-        break
 
-print(f'Current target: {target}')
+def port_range():
+    while True:
+        start_port = int(input("Enter start port: "))
+        end_port = int(input("Enter end port: "))
+        if start_port == end_port:
+            confirm = input(F'Scan port {start_port} [Y/N]: ')
+            if confirm.lower() == 'y':
+                return [start_port, end_port]
+        elif start_port < end_port:
+            confirm = input(F'Scan ports {start_port}-{end_port} [Y/N]: ')
+            if confirm.lower() == 'y':
+                return [start_port, end_port]
+        elif start_port > end_port:
+            print("\nInvalid range.\n")
+
+
+def main():
+    target = target_prompt()
+    range = port_range()
+    print(range)
+
+
+if __name__ == "__main__":
+    main()
