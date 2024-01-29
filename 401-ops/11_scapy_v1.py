0c6b441e86d97ef5b51b27f66af0caba159a6cfa
marcusvno <marcusnogueira23@gmail.com>
2024-01-29 00:06:33 -0500

    401 Ops Challenge 11 complete.

--- 401-ops/11_scapy_v1.py
+++ 401-ops/11_scapy_v1.py
@@ -16,7 +16,9 @@
 #   - If flag 0x14 received, notify user the port is closed.
 #   - If no flag is received, notify the user the port is filtered and silently dropped.
 
-from scapy.all import sr1, IP, ICMP
+from scapy.all import sr1, IP, TCP, send
+import random
+
 
 def target_prompt():
     target = "scanme.nmap.org"
@@ -36,34 +38,45 @@ def target_prompt():
                     print(f'\nCurrent target: {target}')
                     return target  # breaks out of the inner loop
 
-            # break  # break out of the outer loop
-
         elif change_target == 'n':
             print(f'\nCurrent target: {target}')
             return target
 
 
-def port_range():
+def port_range_prompt():
     while True:
         start_port = int(input("Enter start port: "))
         end_port = int(input("Enter end port: "))
-        if start_port == end_port:
-            confirm = input(F'Scan port {start_port} [Y/N]: ')
-            if confirm.lower() == 'y':
-                return [start_port, end_port]
-        elif start_port < end_port:
-            confirm = input(F'Scan ports {start_port}-{end_port} [Y/N]: ')
+        if start_port <= end_port:
+            if start_port == end_port:
+                confirm = input(f'Scan port {start_port} [Y/N]: ')
+            confirm = input(f'Scan ports {start_port}-{end_port} [Y/N]: ')
             if confirm.lower() == 'y':
                 return [start_port, end_port]
         elif start_port > end_port:
-            print("\nInvalid range.\n")
+            print("\nInvalid range. Start port must be less than or equal to end port.\n")
+
+
+def port_scan(target, port_range):
+    choice = input("Do you want to see closed ports [Y/N]: ")
+    for port in range(port_range[0], port_range[1]+1):
+        response = sr1(IP(dst=target)/TCP(sport=(random.randint(2000, 65000)),dport=port, flags="S"), timeout=2, verbose=0) # Send SYN
+        if response is None:
+            print(f'{port} sent no response. Filtered?')
+        elif response.haslayer(TCP):
+            if response.getlayer(TCP).flags == 0x12: # SYN-ACK received. Port open
+                send(IP(dst=target)/TCP(sport=(random.randint(2000, 65000)),dport=port, flags="R"), verbose=0) # Sends RST to close connection
+                print(f'{port} is open.')
+            elif choice.lower() == 'y' and response.getlayer(TCP).flags == 0x14: #RST-ACK received. Port closed
+                print(f'{port} is closed.')
+        else:
+            print(f'{port} sent an unexpected response.')
 
 
 def main():
-    menu_choice = menu()
     target = target_prompt()
-    range = port_range()
-    print(range)
+    port_range = port_range_prompt()
+    port_scan(target,port_range)
 
 
 if __name__ == "__main__":
