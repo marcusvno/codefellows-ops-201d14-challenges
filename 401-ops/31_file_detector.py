ed17ac99000f7dbb7da456a7f019ff10e6c6113c
marcusvno <marcusnogueira23@gmail.com>
2024-02-20 01:51:44 -0500

    401 Ops challenge 31 started. Logging and user input implemented.

---
+++ 401-ops/31_file_detector.py
@@ -0,0 +1,16 @@
+#!/bin/bash/env python3
+
+# Author:                       Marcus Nogueira
+# Date of latest revision:      02/19/2024
+#
+#
+# REQUIREMENTS: 
+#   - Prompt the user to type in a file name to search for.
+#   - Prompt the user for a directory to search in.
+#   - Search each file in the directory by name.
+#       TIP: You may need to perform different commands depending on what OS you’re executing the script on.
+#   - For each positive detection, print to the screen the file name and location.
+#   - # At the end of the search process, print to the screen how many files were searched and how many hits were found.
+#
+#   - The script must successfully execute on both Linux and Windows.
+
