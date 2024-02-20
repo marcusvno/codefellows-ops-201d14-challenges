746cc6420e13ff2d1cd6fc678842d07f7ece0b65
marcusvno <marcusnogueira23@gmail.com>
2024-02-20 02:09:49 -0500

    Prompts finished. Search protocol to come.

--- 401-ops/31_file_detector.py
+++ 401-ops/31_file_detector.py
@@ -4,7 +4,7 @@
 # Date of latest revision:      02/19/2024
 #
 #
-# REQUIREMENTS: 
+# REQUIREMENTS:
 #   - Prompt the user to type in a file name to search for.
 #   - Prompt the user for a directory to search in.
 #   - Search each file in the directory by name.
@@ -14,3 +14,48 @@
 #
 #   - The script must successfully execute on both Linux and Windows.
 
+import logging
+import os
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[
+                        logging.FileHandler('file_detector_log.log'),
+                        logging.StreamHandler()
+                    ])
+
+
+def user_input(target):
+    try:
+        return input(f'Enter {target}: ')
+    except KeyboardInterrupt:
+        logging.warning("User initiated exit.")
+        exit()
+
+
+def directory_input():
+    while True:
+        path_input = user_input("DIRECTORY")
+        path = path_check(path_input)
+        if os.path.exists(path):
+            return path
+        logging.error(f'Directory does not exist.')
+        print("Please enter a valid directory path.")
+
+
+def path_check(path):
+    if path.statswith('~'):
+        return os.path.expanduser(path)
+    else:
+        return path
+
+
+def main():
+    print("\n[  =======  File Detection Script ======= ]")
+    filename = user_input("FILE NAME")
+    directory = directory_input()
+
+
+if __name__ == "__main__":
+    main()
