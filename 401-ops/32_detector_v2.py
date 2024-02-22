c7c97384dd5e28c5fe382d9241d9e9f20e2eb2a6
marcusvno <marcusnogueira23@gmail.com>
2024-02-22 11:11:19 -0500

    Progress on scripts to include hashing and later virustotal comparisons.

--- 401-ops/32_detector_v2.py
+++ 401-ops/32_detector_v2.py
@@ -14,18 +14,24 @@
 import logging
 import os
 
+# Check if a temp folder exists for the log file.
+if not os.path.exists("temp"):
+    os.makedirs("temp")
+
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     handlers=[
-                        logging.FileHandler('file_detector_log.log'),
-                        logging.StreamHandler()
+                        logging.FileHandler('temp/file_detector_log.log'),
+                       # logging.StreamHandler()
                     ])
 
 
 def user_input(target):
     try:
-        return input(f'Enter {target}: ')
+        input_value = input(f'Enter {target}: ')
+        logging.info(f'User input for {target}: {input_value}')
+        return input_value
     except KeyboardInterrupt:
         logging.warning("User initiated exit.")
         exit()
@@ -36,23 +42,43 @@ def directory_input():
         path_input = user_input("DIRECTORY")
         path = path_check(path_input)
         if os.path.exists(path):
+            logging.info(f'Valid directory entered: {path}')
             return path
-        logging.error(f'Directory does not exist.')
+        logging.error('Directory does not exist: %s', path)
         print("Please enter a valid directory path.")
 
 
 def path_check(path):
-    if path.statswith('~'):
-        logging.info('Directory path expanded to %s', os.path.expanduser(path))
-        return os.path.expanduser(path)
+    if path.startswith('~'):
+        expanded_path = os.path.expanduser(path)
+        logging.info('Directory path expanded from %s to %s', path, expanded_path)
+        return expanded_path
     else:
         return path
 
+def search_files(directory, filename):
+    '''Search function that works in Windows (tested with Cmder and Alacritty) and Ubuntu'''
+    file_count = 0
+    files_searched = 0
+    print(f'\nSearching for {filename.upper()} in {directory.upper()} directory\n')
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            files_searched += 1
+            if file == filename:
+                file_count += 1
+                file_path = os.path.join(root, file)
+                logging.info(f'Found: {file_path}')
+                print(f"Found: {file}")
+    logging.info(f"Files searched: {files_searched}, Hits found: {file_count}")
+    print(f"\nFiles searched: {files_searched}, Hits found: {file_count}")
 
 def main():
+    logging.info("[  =======  File Detection Script Initialized ======= ]")
     print("\n[  =======  File Detection Script ======= ]")
     filename = user_input("FILE NAME")
     directory = directory_input()
+    search_files(directory, filename)
+    logging.info("[  =======  File Detection Script Completed ======= ]")
 
 
 if __name__ == "__main__":
