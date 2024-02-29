eede13cbec5172b8ffcb75638656e88135d84aed
marcusvno <marcusnogueira23@gmail.com>
2024-02-29 00:55:12 -0500

    Cookie capture and render complete.

--- 401-ops/37_cookie_capture.py
+++ 401-ops/37_cookie_capture.py
@@ -6,13 +6,16 @@
 # The below Python script shows one possible method to return the cookie from a site that supports cookies.
 
 import requests
+import os
 
 # targetsite = input("Enter target site:") # Uncomment this to accept user input target site
-targetsite = "http://www.whatarecookies.com/cookietest.asp" # Comment this out if you're using the line above
+# Comment this out if you're using the line above
+targetsite = "http://www.whatarecookies.com/cookietest.asp"
 response = requests.get(targetsite)
 cookie = response.cookies
 
-def bringforthcookiemonster(): # Because why not!
+
+def bringforthcookiemonster():  # Because why not!
     print('''
 
               .---. .---.
@@ -28,12 +31,21 @@ def bringforthcookiemonster(): # Because why not!
 
         ''')
 
+
 bringforthcookiemonster()
 print("Target site is " + targetsite)
 print(cookie)
 
 # Add here some code to make this script perform the following:
-# - Send the cookie back to the site and receive a HTTP response
-# - Generate a .html file to capture the contents of the HTTP response
-# - Open it with Firefox
 
+
+# - Send the cookie back to the site and receive a HTTP response
+res = requests.get(targetsite, cookie)
+
+# - Generate a .html file to capture the contents of the HTTP response
+
+with open("temp/cookiemonster.html", 'w', encoding='utf-8') as file:
+    file.write(res.text)
+
+# - Open it with Firefox
+os.system("firefox temp/cookiemonster.html")
