12aad45af6a9f05db87d5507e43a492dd81ef84d
marcusvno <marcusnogueira23@gmail.com>
2024-03-06 21:01:46 -0500

    Testing complete. Script works as intended.

--- 401-ops/43_port_scanner.py
+++ 401-ops/43_port_scanner.py
@@ -9,11 +9,11 @@
 import socket
 
 sockmod = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-TIMEOUT = 10
+TIMEOUT = 5
 sockmod.settimeout(TIMEOUT)
 
 hostip = input("Enter host IPv4: ")
-portno = int(input("Enter port :"))
+portno = int(input("Enter port: "))
 
 def portScanner():
     if sockmod.connect_ex((hostip, portno)) != 0:
